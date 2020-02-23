19b0eeb42e3e16757d9db9d17bc776e6656774ec
Ty Lippe <tylippe94@gmail.com>
2020-02-22 20:15:36 -0500

    cleaned folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Poke from './Components/getpoke'
+
+function App() {
+  return (
+    <div className="App">
+      <Poke />
+    </div>
+  );
+}
+
+export default App;
